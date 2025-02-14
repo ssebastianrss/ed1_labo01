86241108a10b1a33daa783bb77e5b35f3eedf026
Daniel Mejia <rdmejiaa@gmail.com>
2025-02-14 08:16:24 -0600

    Suma

--- src/main/java/ed/lab/SortingTester.java
+++ src/main/java/ed/lab/SortingTester.java
@@ -33,7 +33,7 @@ public class SortingTester<T extends Comparable<T>> {
 
         long sum = durations.stream()
                 .mapToLong(Duration::toMillis)
-                .sum();
+                .sum(); // hola
 
         System.out.printf("\t\tTiempo promedio: %s ms\n", average);
         System.out.println("\t\tSumatoria de tiempo: " + (sum));
