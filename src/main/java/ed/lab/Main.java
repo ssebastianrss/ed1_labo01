fb9b8aaa10f4016533dbd50f4b86b765cbab1f9c
Sebastian <sruanos@correo.url.edu.gt>
2025-02-26 19:19:24 -0600

    Funciones Lambda

--- src/main/java/ed/lab/Main.java
+++ src/main/java/ed/lab/Main.java
@@ -1,11 +1,30 @@
 package ed.lab;
-
+import java.util.Random;
 public class Main {
-    private static final ArrayGenerator<Integer> sortedArrayGenerator = null; // Reemplácelo por una función lambda
+    private static final ArrayGenerator<Integer> sortedArrayGenerator = length -> {
+        Integer[] array = new Integer[length];
+        for (int i = 0; i < length; i++) {
+            array[i] = i;
+        }
+        return array;
+    };
 
-    private static final ArrayGenerator<Integer> invertedArrayGenerator = null; // Reemplácelo por una función lambda
+    private static final ArrayGenerator<Integer> invertedArrayGenerator = length -> {
+        Integer[] array = new Integer[length];
+        for (int i = 0; i < length; i++) {
+            array[i] = length - 1 - i;
+        }
+        return array;
+    };
 
-    private static final ArrayGenerator<Integer> randomArrayGenerator = null; // Reemplácelo por una función lambda
+    private static final ArrayGenerator<Integer> randomArrayGenerator = length -> {
+        Integer[] array = new Integer[length];
+        Random rand = new Random(); // Generador de números aleatorios
+        for (int i = 0; i < length; i++) {
+            array[i] = rand.nextInt(10000); // Asigna un número aleatorio entre 0 y 9999
+        }
+        return array;
+    };
 
     private static final QuickSort<Integer> highPivotQuickSort = null; // Reemplácelo por una referencia a un método
 
